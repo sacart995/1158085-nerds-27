1c45e48a2862397a1f4038224ffb74fd53ff5771
Misha <sacart995@gmail.com>
2020-02-21 15:27:26 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".write-us");
+var popup = document.querySelector(".modal-contacts");
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
